dedbc41527b0cb3f758b8bd500261504b47d282a
fantasy178 <blue-sky-78217@hotmail.com>
2020-11-13 17:12:06 +0800

    加入專案檔案。

---
+++ WPF_MVVM2/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Windows.Controls;
+
+namespace WPF_MVVM2
+{
+    class Person : ViewModelBase
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if(name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if(age != value)
+                {
+                    age = value;
+                    OnPropertyChanged("Age");
+                }
+            }
+        }
+    }
+}
---
+++ WPF_MVVM2/ViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.Windows.Controls;
+
+namespace WPF_MVVM2
+{
+    class ViewModel : ViewModelBase
+    {
+        private ObservableCollection<Person> people = new ObservableCollection<Person>();
+        public ObservableCollection<Person> People
+        {
+            get { return people; }
+            set
+            {
+                if(people != value)
+                {
+                    people = value;
+                    OnPropertyChanged("People");
+                }
+            }
+        }
+
+        public DelegateCommand UseListUpdateCmd { get; set; }
+        public DelegateCommand UseObsUpdateCmd { get; set; }
+
+        public ViewModel()
+        {
+            UseListUpdateCmd = new DelegateCommand(OnUseListUpdateCmdExecuted, (o) => { return true; });
+            UseObsUpdateCmd = new DelegateCommand(OnUseObsUpdateCmdExecuted, (o) => { return true; });
+        }
+
+        private void OnUseListUpdateCmdExecuted(object obj)
+        {
+            List<Person> person = new List<Person>();
+            person.Add(new Person() { Name = "A", Age = 15 });
+            person.Add(new Person() { Name = "B", Age = 30 });
+            this.People = new ObservableCollection<Person>(person);
+        }
+        private void OnUseObsUpdateCmdExecuted(object obj)
+        {
+            ObservableCollection<Person> person = new ObservableCollection<Person>();
+            person.Add(new Person() { Name = "C", Age = 15 });
+            person.Add(new Person() { Name = "D", Age = 30 });
+            this.People = person;
+        }
+    }
+}
